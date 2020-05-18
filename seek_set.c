cc38dfa05c4608c9465b62828c8ba767560fb380
Roman <roman.l.senn@gmail.com>
2020-05-18 07:53:03 +0200

    ...

--- seek_set.c
+++ seek_set.c
@@ -1,10 +1,13 @@
 /* Public domain. */
 
+#include "seek.h"
 #include <sys/types.h>
 #include <unistd.h>
-#include "seek.h"
 
 #define SET 0 /* sigh */
 
-int seek_set(int fd,seek_pos pos)
-{ if (lseek(fd,(off_t) pos,SET) == -1) return -1; return 0; }
+int seek_set(int fd, seek_pos pos) {
+  if (lseek(fd, (off_t)pos, SET) == -1)
+    return -1;
+  return 0;
+}
